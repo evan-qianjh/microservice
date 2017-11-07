65fc52236047f2f6ec27402b54e123d65749b7a7
qianjh <qianjh@qq.com>
2017-11-08 00:50:17 +0800

    add lombok

--- service-customer/service-customer-model/src/main/java/com/qianjh/microservice/domain/Customer.java
+++ service-customer/service-customer-model/src/main/java/com/qianjh/microservice/domain/Customer.java
@@ -1,9 +1,12 @@
 package com.qianjh.microservice.domain;
 
+import lombok.Data;
+
 import java.io.Serializable;
 
 /**
  * @author QianJH
  */
+@Data
 public class Customer implements Serializable{
 }
