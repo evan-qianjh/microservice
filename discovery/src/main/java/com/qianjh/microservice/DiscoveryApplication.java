9da78b4940e93d69aa5589a11ad5ed23890de081
qianjh <qianjh@qq.com>
2017-11-08 23:42:56 +0800

    实现discovery

---
+++ discovery/src/main/java/com/qianjh/microservice/DiscoveryApplication.java
@@ -0,0 +1,17 @@
+package com.qianjh.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author QianJH
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryApplication.class, args);
+	}
+}
